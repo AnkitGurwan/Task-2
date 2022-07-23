da1f669bd072fe7a5c78789b74d31a5187317062
ANKIT GURWAN <109846346+AnkitGurwan@users.noreply.github.com>
2022-07-23 13:51:08 +0530

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,18 @@
+function myFunction() {
+    var input, filter, ul, li, a, i, txtValue;
+    input = Task2.getElementById('username');
+    filter = input.value.toUpperCase();
+    ul = Task2.getElementById("myUL");
+    li = ul.getElementsByTagName('li');
+  
+    // Loop through all list items, and hide those who don't match the search query
+    for (i = 0; i < li.length; i++) {
+      span = li[i].getElementsByTagName("span")[0];
+      txtValue = span.textContent || span.innerText;
+      if (txtValue.toUpperCase().indexOf(filter) > -1) {
+        li[i].style.display = "";
+      } else {
+        li[i].style.display = "none";
+      }
+    }
+  }
